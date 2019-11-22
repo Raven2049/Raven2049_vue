ceefdbeecd9dfe12ab2e077ab343b61c8c147260
raven2049 <1044507066@qq.com>
2019-11-22 17:37:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@ import sideMenu from '@/components/side-menu.vue'
 @Component({
   components: { top, bottom, sideMenu }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  mounted() {}
+}
 </script>
 
 <style lang="stylus"></style>
--- src/components/top.vue
+++ src/components/top.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div class="top flex-center" id="top">
     <span class="title">Raven'blog</span>
@@ -5,30 +6,58 @@
       <span>首页</span>
       <span>我是？</span>
       <span class="fdsf">{{time}}</span>
+      <button @click="change">221312</button>
+      <div class="test" v-if="status.display"></div>
     </span>
   </div>
 </template>
 
 <script lang='ts'>
-export default {
-  data() {
-    return {
-      time: 0
-    }
-  },
-  methods: {
-    timeout() {
-      window.setInterval(() => {
-        setTimeout(() => {
-          (this as any).time++
-        }, 0)
-      }, 1000)
-    }
-  },
-  mounted(): any {
-    (this as any).timeout()
+import { Vue, Emit, Component } from 'vue-property-decorator'
+@Component({
+  mounted() {
+    console.log((this as any).time)
+  }
+})
+export default class Top extends Vue {
+  this = this as any
+  time: Number = 0
+  status: Object = { display: false }
+  @Emit()
+  timeout() {
+    window.setInterval(() => {
+      setTimeout(() => {
+        ;(this as any).time++
+      }, 0)
+    }, 1000)
+  }
+  change() {
+    ;(this as any).status.display = !(this as any).status.display
+  }
+  created() {
+    this.timeout()
   }
 }
+// export default {
+//   data() {
+//     return {
+//       time: 3,
+//       status: { display: false }
+//     }
+//   },
+//   mounted() {
+//     window.setInterval(() => {
+//       setTimeout(() => {
+//         this.time++
+//       }, 0)
+//     }, 1000)
+//   },
+//   methods: {
+//     change() {
+//       this.status.display = !this.status.display
+//     }
+//   }
+// }
 </script>
 
 <style lang='css' scoped>
@@ -37,5 +66,9 @@ export default {
   width: 100%;
   background-color: #776ea7;
   background: linear-gradient(to bottom, #999, #9f9f9f);
+}
+.test {
+  height: 300px;
+  background: black;
 }
 </style>
